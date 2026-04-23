dbc8ccb3c9f1c6512b3a8e9ebdbfe3ac8c197e3c
Kaua vitor <kauavittor220@gmail.com>
2026-04-23 20:31:18 -0300

    Delete ProjetoP3 directory

--- ProjetoP3/Main.py
+++
@@ -1,4 +0,0 @@
-from views.dashboard_view import start
-
-if __name__ == "__main__":
-    start()
--- ProjetoP3/views/dashboard_view.py
+++
@@ -1,5 +0,0 @@
-from controllers.dashboard_controller import show_dashboard
-
-def start():
-    print("DASHBOARD DE COMUNICAÇÃO INTERNA\n")
-    show_dashboard()
